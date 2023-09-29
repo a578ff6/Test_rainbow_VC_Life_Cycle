d788cf5cfe26b009c5974703357030f3c42f525c
wayne tsao <a578ff6@gmail.com>
2023-09-30 05:24:20 +0800

    Initial Commit

---
+++ Test_rainbow_VC_Life_Cycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test_rainbow_VC_Life_Cycle
+//
+//  Created by 曹家瑋 on 2023/9/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
